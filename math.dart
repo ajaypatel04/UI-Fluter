cfb385bffdb54a210710f278d58efd42fce1b08c
Ajay Athineni <156583333+ajaypatel04@users.noreply.github.com>
2025-03-11 22:59:14 +0530

    Add files via upload

---
+++ math.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+void main(){
+int a = 2;
+int b = 3;
+
+var c = a+b;
+print("Sum (a+b) = $c");
+
+var d = a-b;
+print("Diff (a-b) = $d");
+
+var e = a*b;
+print("Sum (a*b) = $e");
+
+var f = a/b;
+print("Sum (a/b) = $f");
+
+}
